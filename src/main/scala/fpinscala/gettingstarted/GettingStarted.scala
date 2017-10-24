da7aff937477340da7797a5c2e72c325eccc363f
Juliusz Nadberezny <nadberezny@gmail.com>
2017-10-24 14:45:24 +0200

    Ex. 2 - IsSorted

--- src/main/scala/fpinscala/gettingstarted/GettingStarted.scala
+++ src/main/scala/fpinscala/gettingstarted/GettingStarted.scala
@@ -1,6 +1,35 @@
 package fpinscala.gettingstarted
 
-object GettingStarted {
+object GettingStarted extends App {
+  def binarySearch[A](as: Array[A], key: A, gt: (A,A) => Boolean): Int = {
+    @annotation.tailrec
+    def go(low: Int, mid: Int, high: Int): Int = {
+      if (low > high) -mid - 1
+      else {
+        val mid2 = (low + high) / 2
+        val a = as(mid2)
+        val greater = gt(a, key)
+        if (!greater && !gt(key,a)) mid2
+        else if (greater) go(low, mid2, mid2-1)
+        else go(mid2 + 1, mid2, high)
+      }
+    }
+    go(0, 0, as.length - 1)
+  }
+
+  def isSorted[A](as: Array[A], gt: (A,A) => Boolean): Boolean = {
+    val last_ix = as.length - 1
+
+    def go(ix: Int, check: Boolean): Boolean = {
+      if (last_ix == ix)
+        check
+      else
+        go(ix + 1, gt(as(ix), as(ix + 1)))
+    }
+
+    go(0, true)
+  }
+
   def fact(n: Int): Int = {
     @annotation.tailrec
     def go(n: Int, acc: Int): Int =
@@ -24,4 +53,6 @@ object GettingStarted {
     else if (n == 1) Vector(0)
     else go(2, Vector(0, 1))
   }
+
+  println(fact(42))
 }
--- src/test/scala/fpinscala/gettingstarted/GettingStartedTest.scala
+++ src/test/scala/fpinscala/gettingstarted/GettingStartedTest.scala
@@ -5,12 +5,31 @@ import GettingStarted._
 
 class GettingStartedTest extends FlatSpec {
 
+  "#binarySearch" should "be polymorphic and return ix of searched key" in {
+    val stringGt = (x: String, y: String) => x > y
+    val strings = Array("a", "b", "c", "d")
+    assert(binarySearch(strings, "c", stringGt) == 2)
+
+    val intGt = (x: Int, y: Int) => x > y
+    val ints = Array(1,2,3,4)
+    assert(binarySearch(ints, 3, intGt) == 2)
+  }
+
   "#fact" should "return a factorial" in {
     assert(fact(5) == 120)
   }
 
   "#fib" should "return a Vector of fibonacci sequence" in {
     assert(fib(5) == Vector(0, 1, 1, 2, 3))
+  }
+
+  "#is_sorted" should "check whether an Array is sorted" in {
+    val sortedInts = Array(1, 2, 3)
+    val intLt = (x: Int, y: Int) => x <= y
+    assert(isSorted(sortedInts, intLt))
+
+    val intGt = (x: Int, y: Int) => x > y
+    assert(!isSorted(sortedInts, intGt))
   }
 
 }
